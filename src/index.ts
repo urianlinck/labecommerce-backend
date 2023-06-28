a80bd18b7ec2d5877e1a32e8e0ff34749bf847ef
urianlinck <urianlinck@gmail.com>
2023-06-28 14:59:25 -0300

    pronto

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,14 @@
-import { user, products } from "./database";
+import { user, products, createUser, getAllUsers, createProduct, getAllProducts, searchProductsByName } from "./database";
 
 console.log(user);
 console.log(products)
+
+createUser("user03", "Tarja Turunen", "naig-tuixi@hotmail.com", "NeMoXD")
+
+console.table(getAllUsers())
+
+createProduct("product03", "presilha", 10, "par de presilha de metal preta com asas de morcego em feltro", "https://http2.mlstatic.com/D_NQ_NP_769732-MLB49303768406_032022-O.webp")
+
+console.table(getAllProducts())
+
+console.table(searchProductsByName("pulseira de spike"))
