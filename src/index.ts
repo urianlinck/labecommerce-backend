e7beff8f86200b20ad833f71f1003d7c3f46787d
urianlinck <urianlinck@gmail.com>
2023-07-18 11:10:59 -0300

    pronto

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { products, users, data2 } from "./database";
 import express, { Request, Response } from "express";
 import cors from "cors";
 import { TProducts, TUsers } from "./types";
+import { db } from "./database-sql/knex";
 
 const app = express();
 
@@ -12,11 +13,33 @@ app.listen(3003, () => {
   console.log("Servidor rodando na porta 3003");
 });
 
-app.get("/ping", (req: Request, res: Response) => {
+app.get("/ping", async (req: Request, res: Response) => {
   res.send("Pong!");
 });
 
-app.get("/users/:id", (req: Request, res: Response) => {
+app.get("/users", async (req: Request, res: Response) => {
+  try{
+    const result = await db.raw(`
+      SELECT * FROM users;
+    `)
+
+    res.status(200). send(result)
+  }catch(error){
+    console.log(error)
+
+    if (req.statusCode === 200){
+      res.status(500)
+    }
+
+    if (error instanceof Error){
+      res.send(error.message)
+    }else {
+      res.send("Erro inesperado.")
+    }
+  }
+})
+
+app.get("/users/:id", async (req: Request, res: Response) => {
   try {
     const id = req.params.id
     const result = users.find((user)=>user.id === id)
@@ -39,21 +62,22 @@ app.get("/users/:id", (req: Request, res: Response) => {
   
 });
 
-app.get("/products", (req: Request, res: Response) => {
+app.get("/products", async (req: Request, res: Response) => {
   try{
     const name = req.query.name as string;
-    const result = products.find((product) =>
-    product.name.toLowerCase().includes(name.toLowerCase())
-  );
-    if(name.length < 1){
-      throw new Error("'Nome do produto' deve possuir no mínimo 1 caractere.")
+    
+    if(!name){
+      const result = await db.raw(`SELECT * FROM products`)
+      return res.status(200).send(result);
     }
-    if(!result){
-    res.status(404)
-    throw new Error("Produto não encontrado.")
-  }
-
-  res.status(200).send(result);
+    if(name.length > 2){
+      const result = await db.raw(`SELECT * FROM products WHERE name LIKE '%${name}%'`)
+      res.status(200).send(result);
+    }
+    if(name.length < 2){
+      res.status(400);
+      throw new Error("'Name' deve ter ao menos 2 caracteres.");
+    }
 
   }catch(error: any){
     console.log(error)
@@ -65,13 +89,13 @@ app.get("/products", (req: Request, res: Response) => {
   }
 });
 
-app.post("/users", (req: Request, res: Response) => {
+app.post("/users", async (req: Request, res: Response) => {
   try{
 
-  const id = req.body.id as string;
-  const name = req.body.name as string;
-  const email = req.body.email as string;
-  const password = req.body.password as string;
+  const id = req.body.id 
+  const name = req.body.name 
+  const email = req.body.email 
+  const password = req.body.password 
 
   if(typeof id !== "string"){
     res.status(400)
@@ -90,27 +114,28 @@ app.post("/users", (req: Request, res: Response) => {
     throw new Error("'Senha' precisa ser uma string.")
   }
 
-  const existingID = users.find((user)=> user.id === id)
+  const [existingID] = await db.raw(`
+    SELECT * FROM users
+    WHERE id = '${id}'
+  `)
 
   if(existingID){
     throw new Error("'ID' já existe.")
   }
 
-  const existingEmail = users.find((user)=>user.email === email)
+  const [existingEmail] = await db.raw(`
+    SELECT * FROM users
+    WHERE email = '${email}'
+  `)
 
   if(existingEmail){
     throw new Error("'Email' já cadastrado.")
   }
 
-  const newUser: TUsers = {
-    id,
-    name,
-    email,
-    password,
-    createdAt: data2.toISOString(),
-  };
-
-  users.push(newUser);
+  await db.raw(`
+  INSERT INTO users(id, name, email, password, created_at)
+  VALUES ('${id}','${name}','${email}','${password}', '${data2.toISOString()}');
+  `)
 
   res.status(201).send("Cadastrado realizado com sucesso!");
 
@@ -125,13 +150,13 @@ console.log(error)
 
 });
 
-app.post("/products", (req: Request, res: Response) => {
+app.post("/products", async (req: Request, res: Response) => {
   try{
-  const id = req.body.id as string;
-  const name = req.body.name as string;
-  const price = req.body.price as number;
-  const description = req.body.description as string;
-  const imageUrl = req.body.imageUrl as string;
+  const id = req.body.id 
+  const name = req.body.name 
+  const price = req.body.price 
+  const description = req.body.description 
+  const imageUrl = req.body.imageUrl 
 
   if(typeof id !== "string"){
     res.status(400)
@@ -154,21 +179,19 @@ app.post("/products", (req: Request, res: Response) => {
     throw new Error("'Imagem' precisa ser uma string.")
   }
 
-  const existingID = products.find((product)=> product.id === id)
+  const [existingID] = await db.raw(`
+    SELECT * FROM products
+    WHERE id = '${id}'
+  `)
 
   if(existingID){
     throw new Error("'ID' já existe.")
   }
 
-  const newProduct: TProducts = {
-    id,
-    name,
-    price,
-    description,
-    imageUrl,
-  };
-
-  products.push(newProduct);
+  await db.raw(`
+    INSERT INTO products (id, name, price, description, image_url)
+    VALUES ('${id}','${name}','${price}','${description}','${imageUrl}');
+  `)
 
   res.status(201).send("Produto cadastrado com sucesso!");
 
@@ -182,7 +205,72 @@ app.post("/products", (req: Request, res: Response) => {
     }
 });
 
-app.put("/products/:id", (req: Request, res: Response) => {
+app.post("/purchases", async (req: Request, res: Response) => {
+  try{
+    const id = req.body.id
+    const buyer = req.body.buyer
+    const totalPrice = req.body.total_price
+    const createdAt = data2.toISOString()
+
+    if(typeof id !== "string"){
+      res.status(400)
+      throw new Error("'ID' precisa ser uma string.")
+    }
+    if(typeof buyer !== "string"){
+      res.status(400)
+      throw new Error("'Comprador' precisa ser uma string.")
+    }
+    if(typeof totalPrice !== "number"){
+      res.status(400)
+      throw new Error("'Preço total' precisa ser um número.")
+    }
+
+    const [idResult] = await db.raw(`
+    SELECT * FROM purchases WHERE id = '${id}'
+    `);
+
+    if(idResult){
+      res.status(400)
+      throw new Error("ID já cadastrada.")
+    }
+
+    const [buyerResult] = await db.raw(`
+    SELECT * FROM purchases WHERE id = '${buyer}'
+    `);
+
+    if(buyerResult){
+      res.status(400)
+      throw new Error("Comprador não encontrado.")
+    }
+
+    for (let i=0; i<=products.length; i++){
+      const [productId] = await db.raw(`
+        SELECT * FROM products WHERE id = '${products[i]}'
+      `);
+
+      if(!productId){
+        res.status(400)
+        throw new Error("Id do produto não encontrado.")
+      }
+    }
+
+    await db.raw(`
+    INSERT INTO purchases (id, buyer, total_price, created_at)
+    VALUES ('${id},'${buyer},'${totalPrice},'${createdAt});
+    `)
+    res.status(201).send("Compra realizada com sucesso.")
+
+  }catch(error:any){
+    console.log(error)
+    
+        if(res.statusCode === 200){
+          res.status(500)
+        }
+        res.send(error.message)
+    }
+})
+
+app.put("/products/:id", async (req: Request, res: Response) => {
   try{
   const id = req.params.id;
 
@@ -192,7 +280,11 @@ app.put("/products/:id", (req: Request, res: Response) => {
   const newDescription = req.body.description as string | undefined;
   const newImageUrl = req.body.imageUrl as string | undefined;
 
-  const productsIndex = products.findIndex((product) => product.id === id);
+  const [productsIndex] = await db.raw(`
+    SELECT * FROM products
+    WHERE ID = '${id}'
+  `)
+  // products.findIndex((product) => product.id === id);
 
   if(productsIndex < 0){
     res.status(404)
@@ -234,17 +326,27 @@ app.put("/products/:id", (req: Request, res: Response) => {
     }
   }
 
-  const product = products.find((product) => product.id === id);
+  const [product] = await db.raw(`
+    SELECT * FROM products
+    WHERE id = '${id}'
+  `)
 
   if (product) {
-    product.id = newId || product.id;
-    product.name = newName || product.name;
-    product.description = newDescription || product.description;
-    product.imageUrl = newImageUrl || product.imageUrl;
+    await db.raw(`
+      UPDATE products
+      SET 
+          id = "${newId || product.id}",
+          name = "${newName || product.name}",
+          description = "${newDescription || product.description}",
+          image_url = "${newImageUrl || product.imageUrl}",
+          price = "${newPrice || product.price}"
+      WHERE
+          id = "${id}";
+    `)
 
-    if (newPrice !== undefined) {
-      product.price = newPrice;
-    }
+  }else {
+    res.status(400)
+    throw new Error("ID não encontrado.")
   }
 
   res.status(200).send("Produto atualizado com sucesso!");
@@ -259,18 +361,24 @@ app.put("/products/:id", (req: Request, res: Response) => {
   }
 });
 
-app.delete("/users/:id", (req: Request, res: Response) => {
+app.delete("/users/:id", async (req: Request, res: Response) => {
   try{
   const id = req.params.id;
 
-  const userIndex = users.findIndex((user) => user.id === id);
+  const [userIndex] = await db.raw(`
+  SELECT * FROM users
+  WHERE id = '${id}'
+  `)
   
   if(userIndex < 0){
     res.status(404)
     throw new Error("Usuário não encontrado.")
   }
   if (userIndex >= 0) {
-    users.slice(userIndex, 1);
+    await db.raw(`
+    DELETE FROM users
+    WHERE id = '${id}'
+    `)
   }
 
   res.status(200).send("User apagado com sucesso!");
@@ -285,18 +393,24 @@ app.delete("/users/:id", (req: Request, res: Response) => {
     }
 });
 
-app.delete("/products/:id", (req: Request, res: Response) => {
+app.delete("/products/:id", async (req: Request, res: Response) => {
   try{
   const id = req.params.id;
 
-  const productsIndex = products.findIndex((product) => product.id === id);
+  const [productsIndex] = await db.raw(`
+  SELECT * FROM products
+  WHERE id = '${id}'
+  `)
 
   if(productsIndex < 0){
     res.status(404)
     throw new Error("Produto não encontrado.")
   }
   if (productsIndex >= 0) {
-    products.slice(productsIndex, 1);
+    await db.raw(`
+    DELETE FROM products
+    WHERE id = '${id}'
+    `)
   }
 
   res.status(200).send("Produto apagado com sucesso!");
@@ -310,3 +424,34 @@ app.delete("/products/:id", (req: Request, res: Response) => {
         res.send(error.message)
     }
 });
+
+app.delete("/purchases/:id", async (req: Request, res: Response) => {
+  try{
+    const id = req.params.id;
+
+    const [purchasesIndex] = await db.raw(`
+    SELECT * FROM purchases
+    WHERE id = '${id}'
+    `)
+
+    if(purchasesIndex < 0){
+      res.status(404)
+      throw new Error("Compra não encontrada.")
+    }
+    if (purchasesIndex >= 0){
+      await db.raw(`
+      DELETE FROM purchases
+      WHERE id = "${id}";
+      `)
+    }
+    res.status(200).send("Compra deletada com sucesso!")
+
+  }catch(error:any){
+    console.log(error)
+    
+        if(res.statusCode === 200){
+          res.status(500)
+        }
+        res.send(error.message)
+    }
+})
